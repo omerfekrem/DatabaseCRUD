5badf3397349f7b309e724f73f7822f5910098e4
Ömer Ekrem <omerfarukekrem@outlook.com>
2021-11-06 00:37:56 +0300

    Add project files.

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumarasi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(612, 195);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(630, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numarası";
+            // 
+            // txtNumarasi
+            // 
+            this.txtNumarasi.Location = new System.Drawing.Point(685, 22);
+            this.txtNumarasi.Name = "txtNumarasi";
+            this.txtNumarasi.Size = new System.Drawing.Size(100, 20);
+            this.txtNumarasi.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(630, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adı";
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(685, 48);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtAdi.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(630, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Soyadı";
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(685, 74);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(100, 20);
+            this.txtSoyadi.TabIndex = 2;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(633, 112);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(152, 23);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Kayıt Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 247);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtSoyadi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumarasi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumarasi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.Button btnEkle;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VerileriTazele();
+        }
+
+        private void VerileriTazele()
+        {
+            //veritabanındaki bilgileri çekip datagrid e yazmamızı sağlayan metod
+
+            //Veritabanına bağlantı kurmamızı sağlar
+            SqlConnection connection = VeriTabaninaBaglan(); 
+
+            //Kurulan bağlantı üzerinden SQL komutunu hazırla
+            SqlCommand command = new SqlCommand("SELECT * FROM Ogrenci", connection); 
+
+            //SQL komutunu çalıştır ve elde edilen değeri reader değişkenine aktar
+            SqlDataReader reader = command.ExecuteReader();
+
+            //Çekilen veriler reader değişkeninde tutuluyor. Buradan dataGrid e aktarmak için gereken hazırlık yapılıyor
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+
+            //reader ve connection kapatılıyor
+            reader.Close();
+            connection.Close();
+
+            //veriler dataGrid e aktarılıyor
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private static SqlConnection VeriTabaninaBaglan()
+        {
+            //Veritabanına bağlanma işlemi select, insert, update vb işlemlerinde yapılması gerekir. Bunun için bağlanma komutlarını tekrar tekrar yazmak yerine metod halina getirmek mantıklı olacaktır.
+            
+            //Bağlantı için kullanılan bağlantı cümlesinin tanımlanması
+            string connectionString = "Data Source=.;Initial Catalog=VT3;Integrated Security=True";
+
+            //bağlantı cümlesi ile bağlantının oluşturulması
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            //bağlantı kapalı durumda ise aç
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+
+            //bağlantı nesnesini çarıldığı ihtiyaç olan yere gönder
+            return connection;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            //kayıt ekleme metodu
+
+            //VT bağlantısı için gerekli olan nesne
+            SqlConnection connection = VeriTabaninaBaglan();
+
+            //SQL komutunun kalıbının ve parametrelerinin hazırlanması
+            SqlCommand command = new SqlCommand("INSERT INTO Ogrenci(Numarasi,Adi,Soyadi) VALUES(@Numarasi,@Adi,@Soyadi)", connection);
+            command.Parameters.AddWithValue("@Numarasi", txtNumarasi.Text);
+            command.Parameters.AddWithValue("@Adi", txtAdi.Text);
+            command.Parameters.AddWithValue("@Soyadi", txtSoyadi.Text);
+
+            //SQL komutunun çalıştır
+            command.ExecuteNonQuery();
+
+            //Bağlantıyı kapat
+            connection.Close();
+
+            //Verileri dataGrid e tekrar çek
+            VerileriTazele();
+        }
+    }
+}
